67bbdbce778fdc1e594e267e1993d5896a8d6448
safe-Kids <syakaizyouhougaku5@gmail.com>
2024-12-02 17:24:07 +0900

    Update reset.js

--- reset.js
+++ reset.js
@@ -9,8 +9,9 @@ function sendResetEmail() {
   const data = new URLSearchParams();
   data.append('action', 'resetPassword');
   data.append('mailAddress', mailAddress);
-
-  fetch('https://script.google.com/macros/s/AKfycbxIza4c7pEr_yBzkzCugqrA1C62PulcyqNODmI3LTSpGfMLuM2PaWfXKlyVokRUmH2sLw/exec', {
+//https://script.google.com/macros/s/AKfycbwr5qd-zL9XJXmct6JRZCifnRGTfQW2Ctud41KmbZJHsv8a5Rrc1z0YSTKfWfng_PZ0Mw/exec
+//https://script.google.com/macros/s/AKfycbwhAlbxb-kHU6K2r_v_I7X9QIAM1kb1ZqidwbogigBTkhWNSb_S6VyHglYjeOv_PPpoFA/exec
+  fetch('https://script.google.com/macros/s/AKfycby3cRLieXHL1xc-aC737dd60YqZLj24TYFH26eqS97gQTY3iCtKHdj_7b7Ios8_YJ149g/exec', {
     method: 'POST',
     body: data
   })
